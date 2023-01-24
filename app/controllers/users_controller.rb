2b5c49764d58119b4704014da95a53f988b7c053
sai kiran <peddisettisai21@gmail.com>
2023-01-24 13:09:39 +0530

    Add login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @user = User.new
   end
@@ -22,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information in was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
